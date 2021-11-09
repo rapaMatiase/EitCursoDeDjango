308d1bb5d760509c328a028416d635a354bfc8bc
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-09 12:41:59 -0300

    Actulizacion para hacer un ejercicio en clases

--- CLASE2/repasoClaseAnterior/urls.py
+++ CLASE2/repasoClaseAnterior/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import VistaSimple, Saludar, SaludarMultiplesPersonas, SaludoCompleto
+from .views import VistaSimple, Saludar, SaludarMultiplesPersonas, SaludoCompleto, UsoDelHtmlConMalasPracticas, usoCorrectoDeUnHtml, cargarHtmlConContexto
 
 urlpatterns = [
     path('intento1/', VistaSimple),
     path( 'saludar/<str:name>', Saludar),
     path('saludoMultiple/<str:name1>/<str:name2>/<int:age>', SaludarMultiplesPersonas),
-    path('saludoCompleto/<str:name>/<str:lastName>/<int:age>/', SaludoCompleto)
+    path('saludoCompleto/<str:name>/<str:lastName>/<int:age>/', SaludoCompleto),
+    path('malaPractica/', UsoDelHtmlConMalasPracticas),
+    path('buenaPractica/', usoCorrectoDeUnHtml),
+    path('contexto/', cargarHtmlConContexto)
+
 ]
--- CLASE2/repasoClaseAnterior/views.py
+++ CLASE2/repasoClaseAnterior/views.py
@@ -25,3 +25,39 @@ Crear una vista que reciba el nombre, apellido y edad de un usuario y lo muestre
 
 def SaludoCompleto(request, name, lastName, age):
     return HttpResponse("Hola {1}, {0} con edad {2}".format(name, lastName, age))
+
+def UsoDelHtmlConMalasPracticas(request):
+    return HttpResponse("""
+        <h1> Titulo <h2>
+        <p style="color:blue;"> Este es un parrafo de ejemplo </p>
+    """)
+    # La mala practica esta en que tengo codigo de HTML envevido en codigo de Python. --> Injeccion de codigo
+
+def usoCorrectoDeUnHtml(request):
+    #return render(request, 'carpeta_nombre_app/template.html')
+    return render(request, 'repasoClaseAnterior/index.html')
+
+def cargarHtmlConContexto(request):
+    #return render(request, 'carpeta_nombre_app/template.html', context={ es un diccionario !!!})
+    return render(
+        request, 
+        'repasoClaseAnterior/indexWithContext.html',
+        {
+            "name" : "Matias", 
+            "lastName" : "Rapa",
+            "status" : "al dia",
+            "Pendientes" : [
+                "Comprar tomate",
+                "Lavar la ropa",
+                "Sacar al perro",
+                "Estudia django"
+            ]
+        })
+
+
+""" 
+Ejercicio 3
+Crear una vista con template que reciba por la url el nombre, apellido y emosion del usuario.
+Mostra en el HTML el nombre y aprellido de color verde si la emosion es felicidad, si no de color rojo si es enojado. Por ultimo, si es triste de color azul.
+3min 12.44AM 
+ """
