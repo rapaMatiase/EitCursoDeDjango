6482d867be6ce72a1d04993e8f42dee257bf5bc6
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-10 10:48:23 -0300

    Actulizacion para alumno

--- CLASE3/temasPendientes/views.py
+++ CLASE3/temasPendientes/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(
+        request,
+        'temasPendientes/index.html',
+    )
+
+
+""" 
+Ejercicio 1
+Crear 2 vistas con sus propios templates con los nombres;
+- Contact
+- Abous us
+3 min 10.50
+"""
 
