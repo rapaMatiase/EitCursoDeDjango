bf527cb4f947e1fd89a68a3b6291939e16e8ee00
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-15 11:32:00 -0300

    Repasando como ahcer urls

--- CLASE4/RespasoClaseAnterior/urls.py
+++ CLASE4/RespasoClaseAnterior/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.FirstView, name="first"),
     path('second/<str:name>/', views.SecondView, name="second"),
-    path('studentList/', views.StudentList, name="StudentList")
+    path('studentList/', views.StudentList, name="StudentList"),
+    path('studentDetail/<int:id>/', views.StudentDetail, name="StudentDetail")
 ]
--- CLASE4/RespasoClaseAnterior/views.py
+++ CLASE4/RespasoClaseAnterior/views.py
@@ -5,16 +5,19 @@ from django.shortcuts import render
 """ Esto por ahora es una base de datos improvisada """
 baseDeDatosImprovisada = [
     {
+        "id" : 1,
         "name" : "Deycis",
         "lastName" : "Mejias",
         "number" : 2
     },
     {
+        "id" : 2,
         "name" : "Federico",
         "lastName" : "Sodo",
         "number" : 8
     },
     {
+        "id" : 3,
         "name" : "Matias",
         "lastName" : "Rapa",
         "number" : 4
@@ -43,4 +46,21 @@ def StudentList(request):
         request,
         'RespasoClaseAnterior/student-list.html',
         {'studentList' : baseDeDatosImprovisada }
+    )
+
+def SearchStudent(id):
+    studentFind = ""
+    for student in baseDeDatosImprovisada:
+        if student["id"]== id:
+            studentFind = student
+    return studentFind
+
+def StudentDetail(request, id):
+    print(id)
+    # Una consulta de bd por un estudiante en particualr.
+    student = SearchStudent(id)
+    return render(
+        request,
+        'RespasoClaseAnterior/student-detail.html',
+        {'student' : student}
     )
