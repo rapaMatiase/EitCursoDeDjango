a8430f36afe668ffaf5b11b0a9da01f28ed48ab2
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-15 10:31:00 -0300

    Enunciado del ejercicio integrador del dia, parte 1

--- CLASE4/RespasoClaseAnterior/urls.py
+++ CLASE4/RespasoClaseAnterior/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FirstView),
-    path('second/<str:name>/', views.SecondView)
+    path('', views.FirstView, name="first"),
+    path('second/<str:name>/', views.SecondView, name="second")
 ]
