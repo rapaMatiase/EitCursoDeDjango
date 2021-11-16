5c31005083aea29147aa3e86cfcdb4a9c37e92dc
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-16 12:11:47 -0300

    Terminando la parte 3 del ejercicio integrador

--- CLASE5/BookStore/urls.py
+++ CLASE5/BookStore/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('editorials/', views.EditorialList, name="editorialList"),
     path('authors/', views.AuthorList, name="authorList"),
     path('book/<int:id>/', views.BookDetail, name="bookDetail"),
-    path('author/<int:id>/', views.AuthorDetail, name="authorDetial"),
+    path('author/<int:id>/', views.AuthorDetail, name="authorDetail"),
     path('editorial/<int:id>/', views.EditorialDetail, name="editorialDetail"),
 
 ]
