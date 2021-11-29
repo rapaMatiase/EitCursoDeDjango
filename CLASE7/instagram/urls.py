45a9bae6b8e149568e7667fde3337897b5a0fdec
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-29 11:26:16 -0300

    Ultima clase ejercicio instagram

--- CLASE7/instagram/urls.py
+++ CLASE7/instagram/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import Home
 
 urlpatterns = [
-    path('home/<int:user_id>/', Home, name="home")
+    path(
+        'home/<int:user_id>/', 
+        Home, 
+        name="home"
+        )
 ]
